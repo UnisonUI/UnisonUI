eaab3001f414227629e677ea5fe5a4bd940ac4de
Pedro Mangabeira <pedro.mangabeiralindekrantz@gmail.com>
2020-05-30 06:11:10 +0100

    style: Remove unused import

--- rest-ui/webpack.dev.js
+++ rest-ui/webpack.dev.js
@@ -1,5 +1,4 @@
 const merge = require('webpack-merge')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 const common = require('./webpack.common')
