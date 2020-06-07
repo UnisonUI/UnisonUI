0632ae7439d45338113473b733759bd3cdd72170
Pedro Mangabeira <pedro.mangabeiralindekrantz@gmail.com>
2020-06-07 23:33:25 +0100

    test: refactor test

--- core/src/test/scala/restui/models/EventSpec.scala
+++ core/src/test/scala/restui/models/EventSpec.scala
@@ -7,20 +7,28 @@ import org.scalatest.prop.TableDrivenPropertyChecks
 import restui.models.Event._
 
 class EventSpec extends AnyFlatSpec with Matchers with TableDrivenPropertyChecks {
-  private val properties = Table(
-    ("event", "json"),
-    (Event.ServiceUp("id", "test", Map("key" -> "value")), """{"event":"serviceUp","id":"id","name":"test","metadata":{"key":"value"}}"""),
-    (Event.ServiceDown("id"), """{"event":"serviceDown","id":"id"}"""),
-    (List(Event.ServiceUp("id", "test", Map("key" -> "value"))),
-     """[{"event":"serviceUp","id":"id","name":"test","metadata":{"key":"value"}}]""")
-  )
   it should "serialise the event as a valid json string" in {
-    forAll(properties) { (input: Any, json) =>
-      val computedJson = input match {
-        case event: Event                  => event.asJson
-        case events: List[Event.ServiceUp] => events.asJson
-      }
-      computedJson.noSpaces shouldBe json
+    val properties = Table(
+      ("event", "json"),
+      (Event.ServiceUp("id", "test", Map("key" -> "value")),
+       """{"event":"serviceUp","id":"id","name":"test","metadata":{"key":"value"}}"""),
+      (Event.ServiceDown("id"), """{"event":"serviceDown","id":"id"}""")
+    )
+
+    forAll(properties) { (event: Event, json) =>
+      event.asJson.noSpaces shouldBe json
     }
   }
+  it should "serialise a list of events as a valid json string" in {
+    val properties = Table(
+      ("event", "json"),
+      (List(Event.ServiceUp("id", "test", Map("key" -> "value"))),
+       """[{"event":"serviceUp","id":"id","name":"test","metadata":{"key":"value"}}]""")
+    )
+
+    forAll(properties) { (events, json) =>
+      events.asJson.noSpaces shouldBe json
+    }
+  }
+
 }
