0e72ce0bc4e7eb5e644e1fd7be26d73f1885d11b
Pedro Mangabeira <pedro.mangabeiralindekrantz@gmail.com>
2020-09-13 22:29:14 +0100

    test: 💍 test restui config file decoder

--- providers/git/src/main/scala/restui/providers/git/git/data/package.scala
+++ providers/git/src/main/scala/restui/providers/git/git/data/package.scala
@@ -4,8 +4,7 @@ import java.io.File
 
 import cats.syntax.functor._
 import io.circe.generic.auto._
-import io.circe.syntax._
-import io.circe.{Decoder, Encoder, HCursor, Json}
+import io.circe.{Decoder, HCursor}
 
 package object data {
   final case class Repository(uri: String,
@@ -19,20 +18,6 @@ package object data {
   final case class RestUI(name: Option[String], specifications: List[Specification])
 
   object RestUI {
-    implicit val encoder: Encoder[RestUI] = (restui: RestUI) => {
-      val specifications = restui.specifications.asJson
-      restui.name match {
-        case None =>
-          Json.obj(
-            "specifications" -> specifications
-          )
-        case Some(name) =>
-          Json.obj(
-            "name"           -> Json.fromString(name),
-            "specifications" -> specifications
-          )
-      }
-    }
     implicit val decoder: Decoder[RestUI] = (cursor: HCursor) =>
       for {
         name           <- cursor.get[Option[String]]("name")
@@ -41,25 +26,15 @@ package object data {
   }
 
   object Specification {
-    implicit val encoder: Encoder[Specification] = Encoder.instance {
-      case specification: UnnamedSpecification => specification.asJson
-      case specification: NamedSpecification   => specification.asJson
-    }
     implicit val decoder: Decoder[Specification] =
       List[Decoder[Specification]](Decoder[UnnamedSpecification].widen, Decoder[NamedSpecification].widen).reduceLeft(_ or _)
   }
 
   object UnnamedSpecification {
-    implicit val encoder: Encoder[UnnamedSpecification] = (specifications: UnnamedSpecification) => Json.fromString(specifications.path)
     implicit val decoder: Decoder[UnnamedSpecification] = (cursor: HCursor) => cursor.as[String].map(UnnamedSpecification(_))
   }
 
   object NamedSpecification {
-    implicit val encoder: Encoder[NamedSpecification] = (specifications: NamedSpecification) =>
-      Json.obj(
-        "name" -> Json.fromString(specifications.name),
-        "path" -> Json.fromString(specifications.path)
-      )
     implicit val decoder: Decoder[NamedSpecification] = (cursor: HCursor) =>
       for {
         name <- cursor.get[String]("name")
---
+++ providers/git/src/test/scala/restui/providers/git/git/data/RestUISpec.scala
@@ -0,0 +1,22 @@
+package restui.providers.git.git.data
+
+import cats.syntax.either._
+import io.circe.yaml.parser
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class RestUISpec extends AnyFlatSpec with Matchers {
+  it should "decode a restui config file" in {
+    val input = """name: "test"
+|specifications:
+| - "file.yaml"
+| - name: "another service"
+|   path: "other.yaml"
+""".stripMargin
+    val restui = parser
+      .parse(input)
+      .flatMap(_.as[RestUI])
+      .valueOr(throw _)
+    restui shouldBe RestUI(Some("test"), UnnamedSpecification("file.yaml") :: NamedSpecification("another service", "other.yaml") :: Nil)
+  }
+}
