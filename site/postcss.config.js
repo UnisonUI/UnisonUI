71c8918b4a868fcd08763dded2d7493d7992d783
Pedro Mangabeira <MaethorNaur@users.noreply.github.com>
2020-12-24 03:55:55 +0000

    ci: 🎡 purgecss

--- site/postcss.config.js
+++ site/postcss.config.js
@@ -1,7 +1,16 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: ['./hugo_stats.json'],
+  defaultExtractor: (content) => {
+    const els = JSON.parse(content).htmlElements
+    return els.tags.concat(els.classes, els.ids)
+  }
+})
+
 module.exports = {
   plugins: [
     require('tailwindcss'),
     require('postcss-nested'),
-    require('autoprefixer')
+    require('autoprefixer'),
+    ...(process.env.HUGO_ENVIRONMENT === 'production' ? [purgecss] : [])
   ]
 }
