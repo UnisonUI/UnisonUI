766166cd6eb987da6ca75223eb21ed4afb5575b5
Pedro Mangabeira <MaethorNaur@users.noreply.github.com>
2020-12-24 04:39:26 +0000

    style: 💄 before

--- site/tailwind.config.js
+++ site/tailwind.config.js
@@ -1,18 +1,6 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
-  important: true, // See https://tailwindcss.com/docs/configuration#important
-  purge: {
-    enabled: process.env.HUGO_ENVIRONMENT === 'production',
-    content: ['./hugo_stats.json'],
-    mode: 'all',
-    options: {
-      defaultExtractor: (content) => {
-        let els = JSON.parse(content).htmlElements;
-        els = els.tags.concat(els.classes, els.ids);
-        return els;
-      }
-    }
-  },
+  purge: false,
   theme: {
     fontFamily: {
       sans: [
