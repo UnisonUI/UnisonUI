cfbd57171c46650d9361028e2536e1b07e8b3add
Pedro Mangabeira <MaethorNaur@users.noreply.github.com>
2021-05-05 17:49:47 +0100

    fix: github token

--- apps/git_provider/config/dev.exs
+++ apps/git_provider/config/dev.exs
@@ -4,8 +4,3 @@ config :git_provider, pull_interval: "15s"
 
 config :git_provider, :git, []
 config :logger, truncate: :infinity
-config :git_provider, :github,
-  api_uri: "https://api.github.com/graphql",
-  api_token: "1ea1ce57fe2f2d2f523bd62954cfc96d7f802092",
-  polling_interval: "15s",
-  repositories: ["MaethorNaur/restui-.+"]
