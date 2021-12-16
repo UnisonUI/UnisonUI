fecba507a7eb0435ee5d84733e22107241f394b2
Pedro Mangabeira <MaethorNaur@users.noreply.github.com>
2021-12-16 22:48:46 +0000

    fix raft call

--- apps/services/lib/services/storage/raft.ex
+++ apps/services/lib/services/storage/raft.ex
@@ -2,6 +2,7 @@ defmodule Services.Storage.Raft do
   require Logger
   alias Services.State
 
+  @dialyzer :no_return
   @behaviour Services.Storage
 
   @spec alive?() :: boolean()
@@ -9,7 +10,7 @@ defmodule Services.Storage.Raft do
 
   @spec available_services :: {:ok, [Services.t()]} | {:error, term()}
   def available_services do
-    case :ra.local_query(:unisonui, &State.available_services/1) do
+    case :ra.local_query(ra_server_id(), &State.available_services/1) do
       {:ok, {_, services}, _} -> {:ok, services}
       {:timeout, _} -> {:error, :timeout}
       error -> error
@@ -18,7 +19,7 @@ defmodule Services.Storage.Raft do
 
   @spec service(id :: String.t()) :: {:ok, Services.t()} | {:error, term()}
   def service(id) do
-    case :ra.local_query(:unisonui, &State.service(&1, id)) do
+    case :ra.local_query(ra_server_id(), &State.service(&1, id)) do
       {:ok, {_, nil}, _} -> {:error, :not_found}
       {:ok, {_, service}, _} -> {:ok, service}
       {:timeout, _} -> {:error, :timeout}
@@ -28,7 +29,7 @@ defmodule Services.Storage.Raft do
 
   @spec dispatch_events(event :: [Services.Event.t()]) :: :ok | {:error, :timeout | term()}
   def dispatch_events(events) do
-    case :ra.process_command(:unisonui, {:events, events}) do
+    case :ra.process_command(ra_server_id(), {:events, events}) do
       {:error, reason} ->
         {:error, reason}
 
@@ -39,4 +40,6 @@ defmodule Services.Storage.Raft do
         :ok
     end
   end
+
+  defp ra_server_id, do: {:unisonui, node()}
 end
