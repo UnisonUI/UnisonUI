a808014d7f7c4a5d4bb59a15855926c2d00d89fe
Pedro Mangabeira <MaethorNaur@users.noreply.github.com>
2022-02-17 20:13:27 +0000

    clean up

--- apps/unison_ui/lib/unison_ui/services/supervisor.ex
+++ apps/unison_ui/lib/unison_ui/services/supervisor.ex
@@ -1,9 +1,9 @@
 defmodule UnisonUI.Services.Supervisor do
-  use Elixir.Supervisor
-  @spec start_link() :: {:ok, pid} | {:error, {:already_started, pid} | {:shutdown, term} | term}
-  def start_link, do: Elixir.Supervisor.start_link(__MODULE__, :ok, name: __MODULE__)
+  use Supervisor
+  @spec start_link() :: Supervisor.on_start()
+  def start_link, do: Supervisor.start_link(__MODULE__, :ok, name: __MODULE__)
 
-  def init(:ok), do: child_spec() |> Elixir.Supervisor.init(strategy: :one_for_one)
+  def init(:ok), do: Supervisor.init(child_spec(), strategy: :one_for_one)
 
   defp child_spec,
     do: [
