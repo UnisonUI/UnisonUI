518274ff0790ae2270711517f5b936c0a30027a2
Pedro Mangabeira <MaethorNaur@users.noreply.github.com>
2022-03-06 10:13:41 +0000

    npm

--- lib/mix/tasks/npm_install.ex
+++
@@ -1,8 +0,0 @@
-defmodule Mix.Tasks.Npm.Install do
-  use Mix.Task
-
-  def run(_) do
-       webapp = ["apps", "unison_ui", "webapp"] |> Path.join()|> Path.expand()
-    System.cmd("npm", ["install"], cd: webapp)
-  end
-end
