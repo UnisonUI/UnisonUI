470d6f4a8db3460512583a6ca3f2067d6fdd06e4
Pedro Mangabeira <MaethorNaur@users.noreply.github.com>
2022-08-28 13:23:26 +0100

    move consul to a branch

--- apps/consul_provider/test/consul_provider_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule ConsulProviderTest do
-  use ExUnit.Case
-  doctest ConsulProvider
-
-  test "greets the world" do
-    assert ConsulProvider.hello() == :world
-  end
-end
--- mix.exs
+++ mix.exs
@@ -14,7 +14,6 @@ defmodule Unisonui.MixProject do
     do: [
       git_provider: :permanent,
       container_provider: :permanent,
-      consul_provider: :permanent,
       webhook_provider: :permanent
     ]
 
@@ -53,7 +52,6 @@ defmodule Unisonui.MixProject do
         unisonui_git_provider: provider_release(:git_provider),
         unisonui_container_provider: provider_release(:container_provider),
         unisonui_webhook_provider: provider_release(:webook_provider),
-        unisonui_consul_provider: provider_release(:consul_provider),
         unisonui: [
           steps: [&npm_deploy/1, :assemble],
           applications: all_apps(),
